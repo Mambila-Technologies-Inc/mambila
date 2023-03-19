27358c797350a1671f1d7df48673395afe95b9a5
anie308 <aniebietabasiobo@gmail.com>
2023-03-19 22:50:58 +0100

    commit

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -2,7 +2,7 @@ import { NextPage } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import { FiArrowRight } from 'react-icons/fi';
+import { BsArrowRight } from 'react-icons/bs';
 
 const Services: NextPage = () => {
   return (
@@ -12,141 +12,33 @@ const Services: NextPage = () => {
           <p className='font-[800] text-[48px] text-[#000000] leading-[43px] font-Mulish'>The industries <br /> we service.</p>
           <button>View all industries</button>
         </div>
-        <div className='grid lg:grid-cols-3 mt-[20px] gap-[40px]'>
+        <div className='grid lg:grid-cols-2 mt-[40px] gap-[40px]'>
           {
-            services.map(({id, title, content})=> (
-              <div key={id} className='shadow rounded-[20px] p-[20px] bg-white text-[#000000]'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, soluta fuga. Ullam doloremque, minus maiores, doloribus accusamus vitae esse harum tempore libero, corrupti expedita recusandae dolor molestiae in repellat fuga?</div>
+            services.map(({id, title, content, image})=> (
+              <div key={id} className='shadow rounded-[20px]  bg-white '>
+              <div className='p-[20px]'>
+              <p className='text-[#000000] font-[700] font-Mulish text-[28px]'>{title}</p>
+              <p className='mt-[13px] font-Montserrat text-[16px] font-[400] text-[#000000]'>{content}</p>
+              <Link href='/' className='text-[#0500FC] font-[800] text-[18px] font-Mulish flex items-center'>
+                <p>Learn More</p>
+              <BsArrowRight/>
+              </Link>
+              </div>
+              <div className="relative w-full h-[200px]   rounded-b-[20px]">
+              <Image
+                src={image}
+                alt="thumbnail"
+                objectFit="cover"
+                layout="fill"
+                className="rounded-b-[20px]"
+              />
+            </div>
+              </div>
             ))
           }
         </div>
       </div>
-      {/* <div className="relative bg-lightGray">
-        <div className="container mx-auto pb-20">
-          <div className="flex items-center justify-between py-20">
-            <div className="max-w-sm w-1/2">
-              <h1 className="max-w-xl text-5xl font-[800] text-center font-Mulish md:text-5xl md:text-left">
-                The industries we service.
-              </h1>
-            </div>
-            <div>
-              <button className="rounded-[10px] text-white mr-20 font-Mulish bg-[blue] py-2 px-5">
-                View all industries
-              </button>
-            </div>
-          </div>
-          <div className="flex flex-wrap gap-8 justify-items-start">
-            <div className="card w-1/4 lg:w-96 bg-base-100 shadow-xl">
-              <div className="card-body">
-                <h2 className="card-title font-Mulish font-[800] text-4xl">
-                  Finance
-                </h2>
-                <p className="text-[16px] space-y-5 font-Montserrat">
-                  We are creating practical solutions to address everyday health
-                  issues that people face, such as fitness trackers, tools to
-                  promote a healthy lifestyle, and better early disease
-                  diagnosis.
-                </p>
-                <Link
-                  href="/"
-                  className="flex items-center gap-1 text-[blue] font-Mulish font-bold text-[20px]"
-                >
-                  Learn more <FiArrowRight />
-                </Link>
-              </div>
-              <div className="">
-                <Image
-                  src="/images/img3.png"
-                  alt="Shoes"
-                  className="w-full h-[150px]"
-                  width={200}
-                  height={100}
-                />
-              </div>
-            </div>
-            <div className="card w-1/4 lg:w-96 bg-base-100 shadow-xl">
-              <div className="card-body">
-                <h2 className="card-title font-Mulish font-[800] text-4xl">
-                  Energy
-                </h2>
-                <p className="text-[16px] space-y-5 font-Montserrat">
-                  Through MT Renewable Energy, we are developing clean energy
-                  resources to meet industrial, commercial, and domestic needs
-                  in underserved areas of emerging communities.
-                </p>
-                <Link
-                  href="/"
-                  className="flex items-center gap-1 text-[blue] font-Mulish font-bold text-[20px]"
-                >
-                  Learn more <FiArrowRight />
-                </Link>
-              </div>
-              <div className="">
-                <Image
-                  src="/images/img4.png"
-                  alt="Shoes"
-                  className="w-full h-[150px]"
-                  width={200}
-                  height={100}
-                />
-              </div>
-            </div>
-            <div className="card w-1/4 lg:w-96 bg-base-100 shadow-xl">
-              <div className="card-body">
-                <h2 className="card-title font-Mulish font-[800] text-4xl">
-                  Healthcare
-                </h2>
-                <p className="text-[16px] space-y-5 font-Montserrat">
-                  We are creating practical solutions to address everyday health
-                  issues that people face, such as fitness trackers, tools to
-                  promote a healthy lifestyle, and better early disease
-                  diagnosis.
-                </p>
-                <Link
-                  href="/"
-                  className="flex items-center gap-1 text-[blue] font-Mulish font-bold text-[20px]"
-                >
-                  Learn more <FiArrowRight />
-                </Link>
-              </div>
-              <div className="">
-                <Image
-                  src="/images/img5.png"
-                  alt="Shoes"
-                  className="w-full h-[150px]"
-                  width={200}
-                  height={100}
-                />
-              </div>
-            </div>
-            <div className="card w-1/4 lg:w-96 bg-base-100 shadow-xl">
-              <div className="card-body">
-                <h2 className="card-title font-Mulish font-[800] text-4xl">
-                  Agriculture
-                </h2>
-                <p className="text-[16px] space-y-5 font-Montserrat">
-                  Mambila Technologies is Africa’s leading Integrated Payments
-                  and Digital Commerce Solutions company,
-                </p>
-                <Link
-                  href="/"
-                  className="flex items-center gap-1 text-[blue] font-Mulish font-bold text-[20px]"
-                >
-                  Learn more <FiArrowRight />
-                </Link>
-              </div>
-              <div className="">
-                <Image
-                  src="/images/img6.png"
-                  alt="Shoes"
-                  className="w-full h-[150px]"
-                  width={200}
-                  height={100}
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */}
+      
     </>
   );
 };
@@ -158,22 +50,29 @@ const services = [
   {
     id: 1,
     title: 'Agriculture',
-    content: "Mambila Technologies is one of Africa's leading provider of integrated payment and digital commerce solutions. We use expertise, cutting-edge innovations, and technologies to provide exceptional financial solutions."
+    content: "Mambila Technologies is one of Africa's leading provider of integrated payment and digital commerce solutions. We use expertise, cutting-edge innovations, and technologies to provide exceptional financial solutions.",
+    image: '/images/img6.png'
   },
   {
     id: 2,
     title: 'Finance',
-    content: "Mambila Technologies is one of Africa's leading provider of integrated payment and digital commerce solutions. We use expertise, cutting-edge innovations, and technologies to provide exceptional financial solutions."
+    content: "Mambila Technologies is one of Africa's leading provider of integrated payment and digital commerce solutions. We use expertise, cutting-edge innovations, and technologies to provide exceptional financial solutions.",
+    image: '/images/img3.png'
+
   },
   {
     id: 3,
     title: 'Energy',
-    content: "Mambila Technologies is one of Africa's leading provider of integrated payment and digital commerce solutions. We use expertise, cutting-edge innovations, and technologies to provide exceptional financial solutions."
+    content: "Mambila Technologies is one of Africa's leading provider of integrated payment and digital commerce solutions. We use expertise, cutting-edge innovations, and technologies to provide exceptional financial solutions.",
+    image: '/images/img4.png'
+
   },
   {
     id: 4,
     title: 'Healthcare',
-    content: "Mambila Technologies is one of Africa's leading provider of integrated payment and digital commerce solutions. We use expertise, cutting-edge innovations, and technologies to provide exceptional financial solutions."
+    content: "Mambila Technologies is one of Africa's leading provider of integrated payment and digital commerce solutions. We use expertise, cutting-edge innovations, and technologies to provide exceptional financial solutions.",
+    image: '/images/img5.png'
+
   },
   
 ]
